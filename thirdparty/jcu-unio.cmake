a6666795ec29b487074269c35f0fbc17c5a7ec04
jichan <joseph@jc-lab.net>
2021-09-25 01:21:17 +0900

    fix: jcu-unio

--- thirdparty/jcu-unio.cmake
+++ thirdparty/jcu-unio.cmake
@@ -1,7 +1,7 @@
 if (NOT JCU_UNIO_FETCH_CONTENT_OPTIONS)
     set(JCU_UNIO_FETCH_CONTENT_OPTIONS
             GIT_REPOSITORY https://github.com/jc-lab/jcu-unio.git
-            GIT_TAG        95bb7e2ddcd0dabb3025ae1f6e62749bbdbddce6
+            GIT_TAG        c385dd0a07dbfe5055db6566201a2923a174545b
             )
 endif ()
 
